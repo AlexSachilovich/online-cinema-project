e8d8cdc2d4f21b141566d22f244756d390ba50a9
Schilovich Alexander <sachilovich.alex@gmail.com>
2022-11-29 16:42:41 +0300

    setting shadow root

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import { Component } from "./core";
+import "./components/atoms/typography/Headings/H1";
 
 export class App extends Component {
-
   render() {
-    return (
-      `<h1>My app</h1>`
-    )
+    return `
+    <it-h1>
+      <div>hello1</div>
+      <div>hello2</div>
+      <div>hello3</div>
+      <div>hello4</div>
+      <div>hello</div>
+      <div>hello</div>
+      <div>hello</div>
+      <div>hello2345345</div>
+    </it-h1>`;
   }
 }
 
